6c044142f57eadd9a3fffd667c6ba89e37916434
Zhraa-Qassem <103251941+Zhraa-Qassem@users.noreply.github.com>
2022-04-09 19:04:35 +0300

    Create task1.dart

---
+++ Dart_pass/task1.dart
@@ -0,0 +1,12 @@
+main() {
+const hour = DateTime.now( ).hour;
+print (hour );
+}
+ /*This constructor isn’t a constant constructor 
+Const variables must be a constant value ( time is a variable) */
+
+
+//maybe try it like this 
+/* var hour = DateTime.now( ).hour;
+print (hour );
+}/*
